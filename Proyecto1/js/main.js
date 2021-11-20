d5794220a9bb54d727326ece9ee19333ce155ca8
DeivL <92716594+DeivL@users.noreply.github.com>
2021-11-20 14:09:59 +0100

    Add files via upload

---
+++ Proyecto1/js/main.js
@@ -0,0 +1,8 @@
+alert("work");
+const color = document.querySelector("#color");
+const seleccion = document.querySelector("#salida");
+
+color.addEventListener("input", () => {
+  const value = color.value;
+  salida.innerHTML;
+});
