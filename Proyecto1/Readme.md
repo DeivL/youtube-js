d27a17b93d9be09286154eda7e81a1f0a370429c
DeivL <92716594+DeivL@users.noreply.github.com>
2021-11-22 22:37:22 +0100

    Delete Proyecto1 directory

--- Proyecto1/Readme.md
+++
@@ -1,2 +0,0 @@
-DeivL
-practicando y aprendiendo
--- Proyecto1/js/main.js
+++
@@ -1,8 +0,0 @@
-alert("work");
-const color = document.querySelector("#color");
-const seleccion = document.querySelector("#salida");
-
-color.addEventListener("input", () => {
-  const value = color.value;
-  salida.innerHTML;
-});
